97a57ec4b7aaa696debcb3db42efd1ee15de369f
dssudake <darshansudake555@gmail.com>
2021-12-17 11:15:08 +0530

    Added Network visualizer to show user addition and connection

---
+++ src/components/Home/NetworkGraph/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Graph from "react-graph-vis";
+import { Card } from "react-bootstrap";
+
+const NetworkGraph = ({ network: { key, graph } }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title className="text-center">Network Visualizer</Card.Title>
+        <Graph key={key} graph={graph} style={{ height: "400px" }} />
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default NetworkGraph;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,15 +4,30 @@ import { Row, Col } from "react-bootstrap";
 import "./styles.css";
 import AddUserForm from "../../components/Home/AddUserForm";
 import ConnectUsersForm from "../../components/Home/ConnectUsersForm";
+import NetworkGraph from "../../components/Home/NetworkGraph";
 
 const Home = () => {
   const [userList, setUserList] = useState({});
   const [newUserId, setNewUserId] = useState(1);
   const [userConnections, setUserConnections] = useState({});
+  const [network, setNetwork] = useState({
+    key: 0,
+    graph: {
+      nodes: [],
+      edges: [],
+    },
+  });
 
   const addNewUser = (name) => {
     setUserList({ ...userList, [newUserId]: name });
     setUserConnections({ ...userConnections, [newUserId]: [] });
+    setNetwork({
+      key: network.key + 1,
+      graph: {
+        ...network.graph,
+        nodes: [...network.graph.nodes, { id: newUserId, label: name }],
+      },
+    });
     setNewUserId(newUserId + 1);
   };
 
@@ -20,6 +35,16 @@ const Home = () => {
     setUserConnections({
       ...userConnections,
       [user1]: [...userConnections[user1], user2],
+    });
+    setNetwork({
+      key: network.key + 1,
+      graph: {
+        ...network.graph,
+        edges: [
+          ...network.graph.edges,
+          { from: parseInt(user1), to: parseInt(user2) },
+        ],
+      },
     });
     setNewUserId(newUserId + 1);
   };
@@ -36,6 +61,10 @@ const Home = () => {
           userConnections={userConnections}
           addNewConnection={addNewConnection}
         />
+      </Col>
+
+      <Col lg={8} xxl={6}>
+        <NetworkGraph network={network} />
       </Col>
     </Row>
   );
