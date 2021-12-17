7f81010aa144dfdbdd523c7227bddd8e91bcf875
dssudake <darshansudake555@gmail.com>
2021-12-17 10:54:34 +0530

    Added functionality to connect two different users

---
+++ src/components/Home/ConnectUsersForm/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Card, Form, Button } from "react-bootstrap";
+
+const ConnectUsersForm = ({ userList, userConnections, addNewConnection }) => {
+  const [firstUser, setFirstUser] = useState("");
+  const [secondUser, setSecondUser] = useState("");
+
+  const handleClick = () => {
+    addNewConnection(firstUser, secondUser);
+    setFirstUser("");
+    setSecondUser("");
+  };
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title className="text-center">Connect Users</Card.Title>
+        <Form.Group className="my-3">
+          <Form.Select
+            value={firstUser}
+            onChange={(e) => {
+              setFirstUser(e.target.value);
+              setSecondUser("");
+            }}
+          >
+            <option>Select First User</option>
+            {Object.keys(userList).map((item) => (
+              <option key={item} value={item}>
+                {`[${item}] ${userList[item]}`}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="my-3">
+          <Form.Select
+            value={secondUser}
+            onChange={(e) => setSecondUser(e.target.value)}
+            disabled={!firstUser}
+          >
+            <option>Select Second User</option>
+            {firstUser &&
+              Object.keys(userList).map(
+                (item) =>
+                  firstUser !== item &&
+                  userConnections[firstUser].indexOf(item) === -1 && (
+                    <option key={item} value={item}>
+                      {`[${item}] ${userList[item]}`}
+                    </option>
+                  )
+              )}
+          </Form.Select>
+        </Form.Group>
+        <Button
+          onClick={handleClick}
+          disabled={!(firstUser && secondUser)}
+          className="w-100"
+        >
+          Connect
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ConnectUsersForm;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,20 +3,39 @@ import { Row, Col } from "react-bootstrap";
 
 import "./styles.css";
 import AddUserForm from "../../components/Home/AddUserForm";
+import ConnectUsersForm from "../../components/Home/ConnectUsersForm";
 
 const Home = () => {
   const [userList, setUserList] = useState({});
   const [newUserId, setNewUserId] = useState(1);
+  const [userConnections, setUserConnections] = useState({});
 
   const addNewUser = (name) => {
     setUserList({ ...userList, [newUserId]: name });
+    setUserConnections({ ...userConnections, [newUserId]: [] });
+    setNewUserId(newUserId + 1);
+  };
+
+  const addNewConnection = (user1, user2) => {
+    setUserConnections({
+      ...userConnections,
+      [user1]: [...userConnections[user1], user2],
+    });
     setNewUserId(newUserId + 1);
   };
 
   return (
-    <Row className="snv-home--wrapper justify-content-center">
+    <Row className="snv-home--wrapper justify-content-center py-5">
       <Col md={6} lg={4} xxl={3}>
         <AddUserForm newUserId={newUserId} addNewUser={addNewUser} />
+      </Col>
+
+      <Col md={6} lg={4} xxl={3}>
+        <ConnectUsersForm
+          userList={userList}
+          userConnections={userConnections}
+          addNewConnection={addNewConnection}
+        />
       </Col>
     </Row>
   );
